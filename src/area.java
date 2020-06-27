aabba7324c0568ae94e8753957d9fb4192d38d7d
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-27 09:40:48 -0300

    Files

---
+++ src/area.java
@@ -0,0 +1,11 @@
+public class area {
+
+    public static void main(String[] args) {
+        double raio = 3.4;
+        final double PI = 3.14;
+
+        double area  = PI * raio  * raio;
+        System.out.println("Area: "+ area);
+    }
+
+}

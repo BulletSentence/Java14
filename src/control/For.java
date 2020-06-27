1ae00ed3781f7791b16eca225185b211776cc5b6
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-27 10:46:40 -0300

    for

---
+++ src/control/For.java
@@ -0,0 +1,11 @@
+package control;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }
+    }
+}

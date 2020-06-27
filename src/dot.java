ac70ec76f46f8cd4612e0be0f1f967111a9b2b6b
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-27 09:56:58 -0300

    dot

---
+++ src/dot.java
@@ -0,0 +1,12 @@
+public class dot {
+    public static void main(String[] args) {
+
+        String text = "Bom Dia";
+        String full = "bebê";
+
+        System.out.println(text.toLowerCase());
+        System.out.println(text.equals("Bom Dia"));
+        System.out.println(text.concat(" "+full));
+
+    }
+}

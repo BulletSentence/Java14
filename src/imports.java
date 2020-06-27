1a368cea0588bfe281c3d16840825d82c248b607
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-27 10:00:42 -0300

    date

---
+++ src/imports.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class imports {
+    public static void main(String[] args) {
+
+        String msg = "Date: ";
+
+        Date date = new Date();
+        System.out.println(msg+date);
+
+
+
+
+    }
+}

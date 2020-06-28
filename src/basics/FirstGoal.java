bac10b1736bd4bf16310606846fff31a1c6cd82d
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-28 11:38:43 -0300

    Static challenge

--- src/basics/FirstGoal.java
+++ src/basics/FirstGoal.java
@@ -3,8 +3,13 @@ package basics;
 public class FirstGoal {
 
     static int a = 3;
+    int b = 5;
 
     public static void main(String[] args) {
         System.out.println(a);
+
+        FirstGoal statico = new FirstGoal();
+        System.out.println(statico.b);
+       
     }
 }

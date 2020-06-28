548182f41c2994f66a92b6068f02de65c3fce546
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-28 11:30:14 -0300

    values

---
+++ src/refValues/ValXsRef.java
@@ -0,0 +1,16 @@
+package refValues;
+
+public class ValXsRef {
+
+    public static void main(String[] args) {
+
+        double a = 2;
+        double b = a;
+
+        a++;
+        b--;    
+
+        System.out.println(a + " " + b);
+
+    }
+}

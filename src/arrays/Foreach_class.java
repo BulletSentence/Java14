48b9cbc85d7881a22cc68488cf7e94e27382035e
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-29 11:29:33 -0300

    Array foreach

---
+++ src/arrays/Foreach_class.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Foreach_class {
+    public static void main(String[] args) {
+        double array[] = {1.0, 4.3, 8.4};
+
+        for (double val : array) {
+            System.out.println(val);
+        }
+    }
+}

bc84933744e6c53c7c83037c234f4cd71683c30c
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-29 11:20:14 -0300

    grades array

--- src/arrays/array_class.java
+++ src/arrays/array_class.java
@@ -5,25 +5,30 @@ public class array_class {
 
     public static void main(String[] args) {
 
-        double grades[] = new double[3];
+        double grades[] = new double[4];
         grades[0] = 8.0;
         grades[1] = 3.6;
         grades[2] = 8.9;
+        grades[3] = 10.0;
 
         System.out.println(grades[0]);
         System.out.println(grades[1]);
         System.out.println(grades[2]);
+        System.out.println(grades[3]);
         System.out.println(grades);
         System.out.println(Arrays.toString(grades));
 
         double media = 0;
 
-        for (int loop = 0; loop < grades.length; loop++){
+        for (int loop = 0; loop < grades.length; loop++) {
             System.out.println(grades[loop]);
             media += grades[loop];
         }
-
-        System.out.println("Media: "+media/3);
+        System.out.println("Media: " + media / grades.length);
+        
+        double grades2[] = {7.2, 5.3, 8.1};
+        for (int i = 0; i < grades2.length; i++) {
+            System.out.println(grades2[i]);
+        }
     }
-
 }
